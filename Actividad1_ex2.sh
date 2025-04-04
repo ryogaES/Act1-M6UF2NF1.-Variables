dec762810f15c32d2fde4250f032b2524edd6664
RyogaEs <iwoxter51@gmail.com>
2025-04-04 11:44:37 -0400

    ex2ç

---
+++ Actividad1_ex2.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo "Introduce el nombre de la variable: "
+read nombre_variable
+
+echo "Introduce su valor: "
+read valor_variable
+
+
+echo "El nombre de la variable es: "$nombre_variable "y su valor es: "$valor_variable
