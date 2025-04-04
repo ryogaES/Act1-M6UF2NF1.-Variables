d94f98d03401fa79102fd9d9df18446f90247db3
RyogaEs <iwoxter51@gmail.com>
2025-04-04 11:36:29 -0400

    ex1

---
+++ Actividad1_ex1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#GLOBAL VARIABLES
+echo $HOME
+echo $USER
+echo $SHELL
+echo $PWD
+echo $PATH
+echo $PS1
+echo $PS2
+echo $TERM
+echo $MANPATH
+echo "Script Name: $0"
+
+#SHELL ARGUMENTS
+echo $1
+echo $2
+echo $3
+echo $#
+echo $*
+echo $@
+echo $?
+echo "PID: $$"
